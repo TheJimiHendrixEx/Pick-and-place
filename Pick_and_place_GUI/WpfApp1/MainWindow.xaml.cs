479ea86b04c61db15bc3670515c5eba01fc0d039
unknown <erickleyva426@gmail.com>
2019-02-16 19:27:51 -0800

    Added code for the video camera

--- Pick_and_place_GUI/WpfApp1/MainWindow.xaml.cs
+++ Pick_and_place_GUI/WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Expression.Encoder.Devices;
+using System.Collections.ObjectModel;
 
 namespace WpfApp1
 {
@@ -20,10 +9,43 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Collection<EncoderDevice> VideoDevices { get; set; }
+        public Collection<EncoderDevice> AudioDevices { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
-            int hello = 0;
+
+            this.DataContext = this;
+
+            VideoDevices = EncoderDevices.FindDevices(EncoderDeviceType.Video);
+            AudioDevices = EncoderDevices.FindDevices(EncoderDeviceType.Audio);
+        }
+        public void StartCaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                WebcamViewer.StartPreview();
+            }
+            catch (Microsoft.Expression.Encoder.SystemErrorException ex)
+            {
+                MessageBox.Show("Device is in use by another application");
+            }
+        }
+        private void StopCaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            WebcamViewer.StopPreview();
+        }
+        private void StartRecordingButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Start recording of webcam video to harddisk.
+            WebcamViewer.StartRecording();
+        }
+
+        private void StopRecordingButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Stop recording of webcam video to harddisk.
+            WebcamViewer.StopRecording();
         }
     }
 }
